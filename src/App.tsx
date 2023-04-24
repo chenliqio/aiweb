dd85db54bde54b6f9f80995bac32894d2faadc4e
“karen” <chenliq2022@163.com>
2023-04-24 09:16:32 +0800

    feat(aiweb): first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Case from "./component/Case";
+import Footer from "./component/Footer";
+import Header from "./component/Header";
+import Part2 from "./component/Part2";
+import { Resolve } from "./component/Resolve";
+import Share from "./component/Share";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Part2 />
+      <Resolve />
+      <Share />
+      <Case />
+      <Footer />
+    </>
   );
 }
 
